8b18f8845c35c2d44c73f2d17285f51fced2d371
Liam McLennan <liam.mclennan@gmail.com>
2016-01-06 12:17:16 +1000

    init

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('buildandtest', shell.task([
+  'tsc src/app.tsx --jsx react --outDir build/es6 -t ES6',
+  'babel build/es6 -d build/es5',
+  'browserify build\\es5\\app.js -o build\\bundle.js'
+]));
+
+gulp.task('default', function() {
+    gulp.watch(['src/*.ts', 'src/*.tsx'], ['buildandtest']);
+});
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Math from './math';
+
+console.log(Math.add(3,4));
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('container')
+);
