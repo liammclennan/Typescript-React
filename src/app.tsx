9a7bfc9ba7e48fb8f9203746706dbde7bb260211
Liam McLennan <liam.mclennan@gmail.com>
2016-03-21 20:27:42 +1000

    working with react-router

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Math from './math';
+import { Router, Route, Link, hashHistory, browserHistory  } from 'react-router';
 
 console.log(Math.add(3,4));
 
+var App = React.createClass({
+    render: function () {
+        return <h1>Hello, world!</h1>;
+    }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+    <Router history={window.location.protocol.startsWith('file') ? hashHistory : browserHistory}>
+    <Route path="/" component={App}></Route>
+    </Router>
+  ,
   document.getElementById('container')
 );
